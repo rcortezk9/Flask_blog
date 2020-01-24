6ce1d3fac02fcfaae0337aeecee8decf0d746625
Rene Cortez <rcortezk9@gmail.com>
2020-01-24 11:06:51 -0600

    Added template structure

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Rene Cortez',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 20, 2020'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 21, 2020'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
-    return render_template('home.html')
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
